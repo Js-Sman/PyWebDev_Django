9b15f129f6570606773997e1bdf3536df42b44e4
Js_Sman <js-schuhmann@outlook.de>
2023-12-14 18:03:52 +0100

    Init Django

---
+++ PyWebDev_DemoApplikation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PywebdevDemoapplikationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "PyWebDev_DemoApplikation"
