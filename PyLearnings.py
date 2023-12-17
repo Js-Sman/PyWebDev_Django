e7c413efd1725d4a6e517e5d24c05ecf4cf64d2e
Js_Sman <js-schuhmann@outlook.de>
2023-12-17 19:16:01 +0100

    PyLearnings aktualisiert

---
+++ PyLearnings.py
@@ -0,0 +1,39 @@
+############## Aufgabe 1 #################
+
+# var = print("Hello World")
+# print(var)
+
+############## Aufgabe 2 #################
+#
+# def read_variablen():
+#     # Innerhalb einer Funktion definierte Variablen
+#     # sind auch nur innerhalb der Funktion verfügbar zum lesen und schreiben
+#
+#     # auf Globale Variablen kann innerhalb von Funktionen immer lesen zugegriffen werden
+#
+#     lokale_variable = 1
+#     print(f'Innerhalb der Funktion "read_variablen": {lokale_variable=}')
+#     print(f'Innerhalb der Funktion "read_variablen": {globale_variable=}')
+#
+#
+# def write_variablen():
+#     # Um Globale Variablen innerhalb einer Funktion auch zu beschreiben, muss dies explizit angegeben werden
+#     global globale_variable
+#
+#     globale_variable = 3
+#     print(f'Innerhalb der Funktion "write_variablen": {globale_variable=}')
+#
+#
+#
+# # Alle Variablen die auf der 1. Tab ebene oder nicht innerhalb einer Funktion definiert wurden, sind global
+# globale_variable = 2
+# read_variablen()
+# print(f"Im global Scope: {globale_variable=}")
+# write_variablen()
+# print(f"Im global Scope: {globale_variable=}")
+#
+# print(lokale_variable)  # Diese Variable ist nicht innerhalb vom global Scope definiert
+# # und somit nicht verfügbar für lese oder schreib operationen
+
+############## Aufgabe 3 #################
+
