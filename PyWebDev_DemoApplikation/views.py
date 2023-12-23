ab28eaa8cff04c046730ee1e6d3a9d0a11c64318
Js_Sman <js-schuhmann@outlook.de>
2023-12-23 14:15:39 +0100

    Bootstrap in Base Template eingebungen

--- PyWebDev_DemoApplikation/views.py
+++ PyWebDev_DemoApplikation/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
+def index_learning(request):
     # Mit einfachen "HttpResponse" kann man z.b Text zurück geben
     # Wenn man jedoch eine Seite in HTML zurück liefer möchte muss diese gerendert werden.
 
@@ -14,9 +14,17 @@ def index(request):
     timestring = time.strftime("%H:%M:%S")  # So würde man ein time format selber definieren
     context = {'now': timestring,
                'time': time.localtime(),
-               'list': [2,3,4,5,6,7,8,9,10,11]}  # der kontext wird als dict übergeben
+               'list': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}  # der kontext wird als dict übergeben
     # um im HTML file die keys zu referenzieren
 
     return render(request, 'demo/index_mitBaseVererbung.html', context)  # Mit dieser Zeile gebe ich an
     # wo die HTML seite beschrieben ist → diese wird als Antwort mit dem context
     # gerendert und zurück an den Server geschickt
+
+
+def index(request):
+    timestring = time.strftime("%H:%M:%S")  # So würde man ein time format selber definieren
+    context = {'now': timestring,
+               'time': time.localtime(),
+               'list': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}  # der kontext wird als dict übergeben
+    return render(request, 'demo/index.html', context)
