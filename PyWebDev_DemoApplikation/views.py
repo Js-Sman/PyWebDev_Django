fb50c0e9ee3a26761ec6eba651d4a2a576b2534f
schuhmannje82308 <schuhmannje82308@th-nuernberg.de>
2024-01-08 13:20:21 +0100

    Doc aufgeräumt

--- PyWebDev_DemoApplikation/views.py
+++ PyWebDev_DemoApplikation/views.py
@@ -6,20 +6,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_learning(request):
-    # Mit einfachen "HttpResponse" kann man z.b Text zurück geben
-    # Wenn man jedoch eine Seite in HTML zurück liefer möchte muss diese gerendert werden.
+    """
+    Um eine eigene Seite anzuzeigen, muss eine HTML-Seite gerendert werden
+    Beim rendern muss der auslösende request mit angegeben werden, der Pfad der zum HTML Template führt und es kann
+    ein context Objekt mit übergeben werden
+    Das context-Objekt ist ein Dictionary, welches dynamisch in der View-Funktion befüllt werden kann.
+    Im HTML-Template kann dann über die key's auf die Inhalte des context-Objekts zugegriffen werden
 
-    # Diese Funktion wird ganz normal abgearbeitet, wenn sie Aufgerufen wird
-    # dynamische Inhalte können hier demnach ganz einfach erzeugt werden und der HTML seite mit übergeben werden
+    :return: Es wird eine HTML-Seite mit Inhalten befüllt
+    """
+
     timestring = time.strftime("%H:%M:%S")  # So würde man ein time format selber definieren
     context = {'now': timestring,
                'time': time.localtime(),
-               'list': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}  # der kontext wird als dict übergeben
-    # um im HTML file die keys zu referenzieren
+               'list': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
 
-    return render(request, 'demo/index_mitBaseVererbung.html', context)  # Mit dieser Zeile gebe ich an
-    # wo die HTML seite beschrieben ist → diese wird als Antwort mit dem context
-    # gerendert und zurück an den Server geschickt
+    return render(request, 'demo/index_mitBaseVererbung.html', context)
 
 
 def index(request):
--- WebDev_Django/urls.py
+++ WebDev_Django/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# Das hier stellt die Hauptseite dar die unter dem local host aufgerufen wird
-# von hier aus werden die routings angegeben
-# jede applikation muss hier als url eingetragen werden um diese über die Hauptseite zu erreichen
 urlpatterns = [
     path('demo/', include('PyWebDev_DemoApplikation.urls')),
     path("admin/", admin.site.urls),
